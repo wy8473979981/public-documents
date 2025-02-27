cac1877be3a086adf83c590a6d778d40598405ed
wy1101123893 <1101123893@qq.com>
2025-02-27 15:12:29 +0800

    小程序监听陀螺仪

--- miniprogram-6/miniprogram/pages/gyroscope/gyroscope.ts
+++ miniprogram-6/miniprogram/pages/gyroscope/gyroscope.ts
@@ -3,93 +3,71 @@
  * @Author: wangyang
  * @Date: 2025-02-27 11:30:02
  * @LastEditors: wangyang
- * @LastEditTime: 2025-02-27 13:43:22
+ * @LastEditTime: 2025-02-27 15:09:43
  */
 // pages/gyroscope/gyroscope.ts
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    x: 0,
+    y: 0,
+    z: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-    // 启动陀螺仪
     wx.startGyroscope({
-      	interval:'normal',
-      success: (res) => {
-        console.log('陀螺仪启动成功', res);
-        // 监听陀螺仪数据变化
-        wx.onGyroscopeChange((res) => {
-          console.log('陀螺仪数据变化', res);
-          // res 包含 x, y, z 三个方向的旋转速度
-        });
-      },
-      fail: (err) => {
-        console.error('陀螺仪启动失败', err);
-      }
+      interval: 'game',
+    });
+    wx.onGyroscopeChange((res) => {
+      this.setData({
+        x: res.x,
+        y: res.y,
+        z: res.z,
+      });
     });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
     // 停止陀螺仪
-    wx.stopGyroscope({
-      success: (res) => {
-        console.log('陀螺仪停止成功', res);
-      },
-      fail: (err) => {
-        console.error('陀螺仪停止失败', err);
-      }
-    });
+    wx.stopGyroscope();
+    wx.offGyroscopeChange();
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
