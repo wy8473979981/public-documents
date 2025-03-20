b2d79638c080f2a7f53712c5a982a42959fdfdfe
wy1101123893 <1101123893@qq.com>
2025-03-20 16:48:36 +0800

    hr第一关静态页

--- hr-activity/miniprogram/pages/game1Page/game1Page.ts
+++ hr-activity/miniprogram/pages/game1Page/game1Page.ts
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    show: false,
+    uploadStatus: false,// false:上传失败或者没有上传，true：上传成功
+    loading: false,// false:没有上传，true：上传中
   },
 
   /**
@@ -20,6 +22,57 @@ Page({
    */
   onReady() {
 
+  },
+  onClickShow() {
+    this.setData({ show: true });
+  },
+  onClickHide() {
+    this.setData({ show: false });
+  },
+  onUpload() {
+    // this.chooseImage();
+  },
+
+  chooseImage() {
+    console.log('chooseImage');
+
+    wx.chooseMedia({
+      count: 1, // 最多可以选择的图片张数，默认9
+      mediaType: ['image'], // 可以指定是图片还是视频，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: (res) => {
+        console.log(res);
+
+        // const tempFilePaths = res.tempFiles.map(file => file.tempFilePath);
+        // this.uploadImage(tempFilePaths[0]);
+      },
+      fail: (err) => {
+        console.error('选择图片失败', err);
+      }
+    });
+  },
+
+  uploadImage(filePath: string) {
+    wx.uploadFile({
+      url: 'https://example.com/upload', // 上传的服务器地址
+      filePath: filePath,
+      name: 'file',
+      formData: {
+        'user': 'test'
+      },
+      success: (res) => {
+        const data = res.data;
+        console.log('上传成功', data);
+        this.setData({ uploadStatus: true, loading: false });
+      },
+      fail: (err) => {
+        console.error('上传失败', err);
+        this.setData({ loading: false });
+      }
+    });
+  },
+  goHome() {
+    wx.redirectTo({ url: '/pages/homePage/homePage' });
   },
 
   /**
--- miniprogram-npm/miniprogram/pages/index/index.ts
+++ miniprogram-npm/miniprogram/pages/index/index.ts
@@ -1,56 +1,67 @@
 import { createScopedThreejs } from 'threejs-miniprogram';
 
 Page({
+  data: {
+    show: false,
+  },
+
+  onClickShow() {
+    this.setData({ show: true });
+  },
+
+  onClickHide() {
+    this.setData({ show: false });
+  },
   onReady() {
-    wx.createSelectorQuery()
-      .select('#webgl')
-      .node()
-      .exec((res) => {
-        const canvas = res[0].node;
-        const THREE = createScopedThreejs(canvas);
+    // wx.createSelectorQuery()
+    //   .select('#webgl')
+    //   .node()
+    //   .exec((res) => {
+    //     const canvas = res[0].node;
+    //     const THREE = createScopedThreejs(canvas);
         
-        // 1. 初始化基础Three.js组件
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(
-          75,
-          canvas.width / canvas.height,
-          0.1,
-          1000
-        );
+    //     // 1. 初始化基础Three.js组件
+    //     const scene = new THREE.Scene();
+    //     const camera = new THREE.PerspectiveCamera(
+    //       75,
+    //       canvas.width / canvas.height,
+    //       0.1,
+    //       1000
+    //     );
         
-        // 2. 创建WebGL渲染器
-        const renderer = new THREE.WebGLRenderer({
-          canvas: canvas,
-          antialias: true
-        });
-        renderer.setSize(canvas.width, canvas.height);
+    //     // 2. 创建WebGL渲染器
+    //     const renderer = new THREE.WebGLRenderer({
+    //       canvas: canvas,
+    //       antialias: true
+    //     });
+    //     renderer.setSize(canvas.width, canvas.height);
 
-        // 3. 创建立方体
-        const geometry = new THREE.BoxGeometry(2, 2, 2); // 边长为2的立方体
-        const material = new THREE.MeshNormalMaterial(); // 使用法向材质
-        const cube = new THREE.Mesh(geometry, material);
+    //     // 3. 创建立方体
+    //     const geometry = new THREE.BoxGeometry(2, 2, 2); // 边长为2的立方体
+    //     const material = new THREE.MeshNormalMaterial(); // 使用法向材质
+    //     const cube = new THREE.Mesh(geometry, material);
         
-        // 4. 设置相机位置
-        camera.position.z = 5;
+    //     // 4. 设置相机位置
+    //     camera.position.z = 5;
 
-        // 5. 将立方体加入场景
-        scene.add(cube);
+    //     // 5. 将立方体加入场景
+    //     scene.add(cube);
 
-        // 6. 动画循环
-        const animate = () => {
-          // 使用小程序canvas请求动画帧
-          canvas.requestAnimationFrame(animate);
+    //     // 6. 动画循环
+    //     const animate = () => {
+    //       // 使用小程序canvas请求动画帧
+    //       canvas.requestAnimationFrame(animate);
 
-          // 立方体旋转动画
-          cube.rotation.x += 0.01;
-          cube.rotation.y += 0.01;
+    //       // 立方体旋转动画
+    //       cube.rotation.x += 0.01;
+    //       cube.rotation.y += 0.01;
 
-          // 渲染场景
-          renderer.render(scene, camera);
-        };
+    //       // 渲染场景
+    //       renderer.render(scene, camera);
+    //     };
 
-        // 启动动画
-        animate();
-      });
+    //     // 启动动画
+    //     animate();
+    //   });
   }
 });
