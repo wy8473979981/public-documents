708201040a99ca2e06b49641e01f9bb0c80c1984
wy1101123893 <1101123893@qq.com>
2025-03-21 09:17:02 +0800

    hr-第二个游戏

---
+++ hr-activity/miniprogram/pages/game2Page/data.js
@@ -0,0 +1,141 @@
+export const allQuestionList = [{
+    num: 1,
+    isAnswered: false,
+    isAnswerWrong: false, // true:表示回答错误
+    rightAnswers: 'D.800',
+    question: '1.每年福利年度，符合条件的员工可以获得多少积分/人/年的超级积分？',
+    answerList: [{
+      id: 1,
+      text: 'A.500',
+      icon: 0,
+      correct: false
+    }, {
+      id: 2,
+      text: 'B.600',
+      icon: 0,
+      correct: false
+    }, {
+      id: 3,
+      text: 'C.700',
+      icon: 0,
+      correct: false
+    }, {
+      id: 4,
+      text: 'D.800',
+      icon: 0,
+      correct: true
+    }]
+  },
+  {
+    num: 2,
+    isAnswered: false,
+    isAnswerWrong: false,
+    rightAnswers: 'D.800',
+    question: '2.每年福利年度，符合条件的员工可以获得多少积分/人/年的超级积分？',
+    answerList: [{
+      id: 1,
+      text: 'A.500',
+      icon: 0,
+      correct: false
+    }, {
+      id: 2,
+      text: 'B.600',
+      icon: 0,
+      correct: false
+    }, {
+      id: 3,
+      text: 'C.700',
+      icon: 0,
+      correct: false
+    }, {
+      id: 4,
+      text: 'D.800',
+      icon: 0,
+      correct: true
+    }]
+  },
+  {
+    num: 3,
+    isAnswered: false,
+    isAnswerWrong: false,
+    rightAnswers: 'C.700',
+    question: '3.每年福利年度，符合条件的员工可以获得多少积分/人/年的超级积分？',
+    answerList: [{
+      id: 1,
+      text: 'A.500',
+      icon: 0,
+      correct: false
+    }, {
+      id: 2,
+      text: 'B.600',
+      icon: 0,
+      correct: false
+    }, {
+      id: 3,
+      text: 'C.700',
+      icon: 0,
+      correct: true
+    }, {
+      id: 4,
+      text: 'D.800',
+      icon: 0,
+      correct: false
+    }]
+  },
+  {
+    num: 4,
+    isAnswered: false,
+    isAnswerWrong: false,
+    rightAnswers: 'B.600',
+    question: '4.每年福利年度，符合条件的员工可以获得多少积分/人/年的超级积分？',
+    answerList: [{
+      id: 1,
+      text: 'A.500',
+      icon: 0,
+      correct: false
+    }, {
+      id: 2,
+      text: 'B.600',
+      icon: 0,
+      correct: true
+    }, {
+      id: 3,
+      text: 'C.700',
+      icon: 0,
+      correct: false
+    }, {
+      id: 4,
+      text: 'D.800',
+      icon: 0,
+      correct: false
+    }]
+  },
+  {
+    num: 5,
+    isAnswered: false,
+    isAnswerWrong: false,
+    rightAnswers: 'A.500',
+    question: '5.每年福利年度，符合条件的员工可以获得多少积分/人/年的超级积分？',
+    answerList: [{
+      id: 1,
+      text: 'A.500',
+      icon: 0,
+      correct: true
+    }, {
+      id: 2,
+      text: 'B.600',
+      icon: 0,
+      correct: false
+    }, {
+      id: 3,
+      text: 'C.700',
+      icon: 0,
+      correct: false
+    }, {
+      id: 4,
+      text: 'D.800',
+      icon: 0,
+      correct: false
+    }]
+  }
+]
